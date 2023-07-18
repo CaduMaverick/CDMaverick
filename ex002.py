f49a62bff21c268c70f6b627f1c4db2b9f1c9046
CaduMaverick <58917326+CaduMaverick@users.noreply.github.com>
2023-07-18 10:09:01 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,8 @@
+nome = input('\033[;;44m'
+             'Digite seu nome: '
+             '\033[m')
+print('\033[;;44m'
+      'É um prazer te conhecer,'
+      '\033[;;43m'
+      f' {nome}! '
+      '\033[m')
