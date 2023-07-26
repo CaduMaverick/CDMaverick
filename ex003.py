b6c13ca939c7a7e8615d26818f1cd72f2715e86f
Cadu Maverick <58917326+CaduMaverick@users.noreply.github.com>
2023-07-26 12:15:12 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+print(f'{n1} mais {n2} é igual á {n1+n2}')
