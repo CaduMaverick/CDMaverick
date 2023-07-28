d32b2bdf35deb4df59e2073d37ce27deb8f58681
Cadu Maverick <58917326+CaduMaverick@users.noreply.github.com>
2023-07-28 12:41:57 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número: '))
+print(f'O número que você digitou foi {n}\n'
+      f'Seu sucessor é {n+1}\n'
+      f'Seu antecessor é {n-1}')
---
+++ ex006.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int(input('Digite um número: '))
+print(f'O número que você digitou foi {n}\n'
+      f'Seu dobro é {n*2}\n'
+      f'Seu triplo é {n*3}\n'
+      f'Sua raiz quadrada é {n**(1/2)}')
